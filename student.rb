7c9599025f23e456babbce913f17296e0966ce90
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-09-28 17:58:04 +0300

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,16 @@
 require './person'
-
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom = 1, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.includes?(self)
   end
 end
