86c0c85a3962c1296733abcd3241ef7ac96455ba
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-09-28 11:43:03 +0300

    Create decorator.rb

---
+++ decorator.rb
@@ -0,0 +1,30 @@
+require 'nameable'
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0..10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
